93a1641e1ec7046ee11e64446ae1c55a817f8da6
Savyasach Reddy <savyasach@cse.iitb.ac.in>
2020-10-18 00:38:25 +0530

    :(

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -13,7 +13,7 @@ export class FormComponent implements OnInit {
   message: string="";
   bform: Form;
   myform= this.fb.group({
-    name:['',[Validators.required]],
+    name:['',[Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
     email:['',[Validators.required]],
     feedback: ['',[Validators.required]],
     comment: ['']
